d027fef5c88a55fd7937fdc247557ebc210a9e17
sebalr <larrieu.sebastian@gmail.com>
2020-03-15 20:05:24 -0300

    Sidebar menu Links
    Add links to sidebar menu and active color.
    Original README aux files removed.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,24 +1,14 @@
 <template>
-  <b-menu>
-    <b-menu-list>
-      <b-menu-item icon="information-outline" label="Info"></b-menu-item>
-      <b-menu-item icon="settings" :active="isActive">
-        <template slot="label" slot-scope="props">
-          Administrator
-          <b-icon class="is-pulled-right" :icon="props.expanded ? 'menu-down' : 'menu-up'"></b-icon>
-        </template>
-        <b-menu-item icon="account" label="Users"></b-menu-item>
-        <b-menu-item icon="cash-multiple" label="Payments" disabled></b-menu-item>
-      </b-menu-item>
-      <b-menu-item icon="account" label="My Account">
-        <b-menu-item label="Account data"></b-menu-item>
-        <b-menu-item label="Addresses"></b-menu-item>
-      </b-menu-item>
-    </b-menu-list>
-    <b-menu-list>
-      <b-menu-item label="Expo" icon="link" tag="router-link" target="_blank" to="/expo"></b-menu-item>
-    </b-menu-list>
-  </b-menu>
+  <aside class="menu">
+    <ul class="menu-list">
+      <li>
+        <g-link to="/">Home</g-link>
+      </li>
+      <li>
+        <g-link to="/blog">Posts</g-link>
+      </li>
+    </ul>
+  </aside>
 </template>
 
 <script>
