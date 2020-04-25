ae1a82d93fcd9623e1f918e5f9e1a1e2dbaa694d
sebalr <larrieu.sebastian@gmail.com>
2020-04-25 20:07:28 -0300

    Type effect
    Added a typing effect to index with js recursive functions.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,91 @@
 <!-- Page -->
 <template>
   <Layout>
-    <div class="buttons">
-      <b-button type="is-primary">Primary</b-button>
-      <b-button type="is-success">Success</b-button>
-      <b-button type="is-danger">Danger</b-button>
-      <b-button type="is-warning">Warning</b-button>
-      <b-button type="is-info">Info</b-button>
-      <b-button type="is-link">Link</b-button>
-      <b-button type="is-light">Light</b-button>
-      <b-button type="is-dark">Dark</b-button>
-      <b-button type="is-text">Text</b-button>
-    </div>
+    <section class="container">
+      <h2 class="hi-title">Hello!</h2>
+      <h3 class="im-title">I'm</h3>
+      <label>{{nameLabel}}</label>
+      <span class="cursor">&nbsp;</span>
+    </section>
   </Layout>
 </template>
+
+<script>
+const textArray = ["Sebastian", "a Software Engineer", "a developer"];
+const typingDelay = 150;
+const erasingDelay = 100;
+const newTextDelay = 1500;
+
+let textArrayIndex = 0;
+let charIndex = 0;
+export default {
+  data() {
+    return {
+      nameLabel: ""
+    };
+  },
+  methods: {
+    type() {
+      if (charIndex < textArray[textArrayIndex].length) {
+        this.nameLabel += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(() => this.type(), typingDelay);
+      } else {
+        setTimeout(() => this.erase(), newTextDelay);
+      }
+    },
+    erase() {
+      if (charIndex > 0) {
+        this.nameLabel = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(() => this.erase(), erasingDelay);
+      } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(() => this.type(), typingDelay + 1100);
+      }
+    }
+  },
+  mounted: function() {
+    setTimeout(() => this.type(), 200);
+  }
+};
+</script>
+
+<style scoped>
+.hi-title {
+  font-size: 6rem;
+}
+
+.im-title {
+  font-size: 4rem;
+  display: inline-block;
+  margin-right: 4rem;
+}
+
+span.cursor {
+  display: inline-block;
+  background-color: #ccc;
+  margin-left: 0.1rem;
+  width: 3px;
+  animation: blink 1s infinite;
+}
+/* cursor animations */
+@keyframes blink {
+  0% {
+    background-color: #ccc;
+  }
+  49% {
+    background-color: #ccc;
+  }
+  50% {
+    background-color: transparent;
+  }
+  99% {
+    background-color: transparent;
+  }
+  100% {
+    background-color: #ccc;
+  }
+}
+</style>
